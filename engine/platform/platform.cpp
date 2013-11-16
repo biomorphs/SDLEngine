94af65be93b0707195e7ad051c4ab046c8de68b3
biomorphs <matt.hoyle@gmail.com>
2013-11-16 08:20:22 +0100

    Projects setup (shared properties, kernel + platform project)

--- engine/platform/platform.cpp
+++ engine/platform/platform.cpp
@@ -2,3 +2,15 @@
 SDLEngine
 	Matt Hoyle
 */
+
+#include "platform.h"
+
+Platform::InitResult Platform::Initialise(int argc, char* argv[])
+{
+	return InitOK;
+}
+
+Platform::ShutdownResult Platform::Shutdown()
+{
+	return ShutdownOK;
+}
--- engine/platform/platform.h
+++ engine/platform/platform.h
@@ -2,3 +2,22 @@
 SDLEngine
 	Matt Hoyle
 */
+
+// This class is responsible for initialising any platform stuff
+class Platform
+{
+public:
+	enum InitResult
+	{
+		InitOK,
+		InitFailed,
+	};
+
+	enum ShutdownResult
+	{
+		ShutdownOK
+	};
+
+	static InitResult Initialise(int argc, char* argv[]);
+	static ShutdownResult Shutdown();
+};
