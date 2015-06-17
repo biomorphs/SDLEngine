cf4e543a5cfb52ac4f37cee4deecd6731a82d353
biomorphs <matt.hoyle@gmail.com>
2015-06-17 17:12:22 +0200

    Asserts disabled in release

--- engine/public/kernel/assert.h
+++ engine/public/kernel/assert.h
@@ -6,4 +6,12 @@ SDLEngine
 
 #include "log.h"
 
-#define SDE_ASSERT(condition, ...)	if( !(condition) )	{ SDE_LOGC(Engine,"Assertion Failed!\r\n\t" #condition "\r\n\t" ##__VA_ARGS__); __debugbreak(); }
+#ifdef SDE_DEBUG
+
+	#define SDE_ASSERT(condition, ...)	if( !(condition) )	{ SDE_LOGC(Engine,"Assertion Failed!\r\n\t" #condition "\r\n\t" ##__VA_ARGS__); __debugbreak(); }
+
+#else
+
+	#define SDE_ASSERT(...)
+
+#endif
